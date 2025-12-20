fae92d7e8dbf41500c8be3e2a6213f249cebc3ba
karthick <itskarthi4@gmail.com>
2025-12-20 18:22:43 +0530

    from main

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -5,7 +5,6 @@ package com.demo;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("hello added ");
+       System.out.println();
     }
 }

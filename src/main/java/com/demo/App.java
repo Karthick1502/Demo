7a42c7416edf996969f15f7494baec635de6f9c4
karthick <itskarthi4@gmail.com>
2025-12-20 20:09:24 +0530

    updated from AM102

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -5,12 +5,12 @@ package com.demo;
  */
 public class App {
     public static void main(String[] args) {
-    	System.out.println("main2");
     	System.out.println("this is AM102 changes");
     	System.out.println("this is AM102 changes 2nd line ");
     	System.out.println("this is AM102 changes 3nd line ");
     	System.out.println("this is AM102 changes 4nd line ");
+    	System.out.println("this is AM102 changes 5nd line ");
+    	System.out.println("this is AM102 changes 6nd line ");
     	System.out.println("main changes");
-   
     }
 }

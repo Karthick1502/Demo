da83a2671306c885d17a31d95768e9489a868880
karthick <itskarthi4@gmail.com>
2025-12-20 19:54:19 +0530

    adding main

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -5,8 +5,6 @@ package com.demo;
  */
 public class App {
     public static void main(String[] args) {
-    	System.out.println("this is AM102 changes");
-    	System.out.println("this is AM102 changes 2nd line ");
-    	System.out.println("main changes");
+    	System.out.println("main2");
     }
 }
